05d20899f5246b23351ab9453beb9034a86cd125
Danddie Porras <qualityassurance@kingsland.io>
2022-04-19 19:55:11 +0800

    small change to test again

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ const Home: NextPage = () => {
   return (
     <div className="container max-w-4xl mx-auto pt-16 md:pt-32 text-center break-normal">
       <p className="text-red-500 font-extrabold text-3xl md:text-5xl">
-        Ghostwind
+        Ghostwind CSS
       </p>
       <p className="text-xl md:text-2xl text-gray-500"> Welcome to my Blog </p>
     </div>
